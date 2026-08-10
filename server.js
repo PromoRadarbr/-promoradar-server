18ede8d308c70b1360fd465a03c30f1788bfbb7c
PromoRadarbr <cassiomaues1@gmail.com>
2026-08-09 22:55:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ app.get("/", (req, res) => {
   res.send("PromoRadar online!");
 });
 
+// Inicia a autorização do Mercado Livre
+app.get("/auth", (req, res) => {
+  const clientId = process.env.ML_CLIENT_ID;
+  const redirectUri = process.env.ML_REDIRECT_URI;
+
+  const authUrl =
+    `https://auth.mercadolivre.com.br/authorization` +
+    `?response_type=code` +
+    `&client_id=${clientId}` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+  res.redirect(authUrl);
+});
+
+// Recebe as notificações do Mercado Livre
 app.post("/notifications", (req, res) => {
   console.log("Notificação recebida:", req.body);
   res.sendStatus(200);
